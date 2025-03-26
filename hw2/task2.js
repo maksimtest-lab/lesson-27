415e9e7b17801e61bf313e0c0079fd1fbbd60a5c
Maksim Alipau <maxanotest@gmail.com>
2025-03-26 19:51:14 +0300

    feat: add a style for task 2 part 2

---
+++ hw2/task2.js
@@ -0,0 +1,10 @@
+// Задание на XSS атаку
+
+// Создайте простую веб-страницу, на которой пользователь может ввести свое имя в форму. Напишите скрипт, который вставит имя пользователя на страницу без экранирования. Попробуйте ввести вредоносный скрипт в поле для имени и убедитесь, что он будет выполнен.
+
+const input = document.querySelector('input#name');
+const output = document.querySelector('.output');
+
+input.addEventListener('input', () => {
+    output.textContent = input.value;
+});
